2524a45bf928bc31b37fe443f1e481a0e857d332
ManuelBenini <manuelbenini1905@gmail.com>
2022-06-07 15:23:18 +0200

    Snack 4 terminato

--- Snack 4/index.php
+++ Snack 4/index.php
@@ -5,6 +5,21 @@
     Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta 
   */
 
+  function getUniqueRandom($min, $max, $nItem){
+    $newArray = [];
 
+    while(count($newArray) < $nItem){
+      $number = rand($min, $max);
+
+      if(!in_array($number, $newArray)){
+        $newArray[] = $number;
+      }
+    };
+    return $newArray;
+  };
+
+  $random_numbers = getUniqueRandom(1, 15, 15);
+
+  var_dump($random_numbers);
 
 ?>
